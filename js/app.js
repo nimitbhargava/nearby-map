91aa7a6cb8d14fdb5d700f65ca3026f990ad156c
Nimit Bhargava <nimitbhargava@gmail.com>
2017-09-10 19:57:26 +0530

    Creating a basic marker

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 var map;
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
+    var myLatLng = {lat: -34.397, lng: 150.644};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
         zoom: 8
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Hello World!'
     });
 }
